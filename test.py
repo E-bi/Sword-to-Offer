1184b46e57958c5e8951ea04829b2357b7f4c315
Lex <luohai2233@163.com>
2018-08-07 14:12:05 +0800

    Update

--- test.py
+++ test.py
@@ -1,12 +1,7 @@
-def FindMaxSum(a):
-    len_a = len(a)
-    buff = []
-    total = 0
-    for i in range(len_a):
-        total = total+a[i]
-        if total<a[i]:
-            buff.append(total)
-            total = a[i]
-    print(max(buff))
-a= [1,-2,3,10,-4,7,2,-5]
-FindMaxSum(a)
+class C(object):
+    pass
+
+class B:
+    pass
+print(dir(C))
+print(dir(B))
