da572d7fe25caeaec136f5b7c09439b49c37fba8
Lex <luohai2233@163.com>
2020-06-16 11:23:04 +0800

    完成快排

--- bin/quicksort.py
+++ bin/quicksort.py
@@ -1,7 +1,14 @@
 #快速排序
 def quickSort(alist):
-    pass
+    if len(alist) < 2:
+        return alist
+    mid = alist[0]
+    smaller = [item for item in alist if item < mid]
+    bigger = [item for item in alist if item > mid]
+    equa = [item for item in alist if item == mid]
+    return quickSort(smaller) + equa + quickSort(bigger)
 
 
-test = [1,3,2,5,4,3,2,5,7,5,3]
+# test = [1,3,2,5,4,3,2,5,7,5,3]
+test = [7,6,5,4,3,2,1]
 print(quickSort(test))
